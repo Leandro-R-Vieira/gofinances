fdff15145643c67957e3c551189f912a4fde5606
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-07-06 18:40:11 -0300

    second commit

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Container, Title } from './styles'
+
+
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName
+
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/103189479?v=4'}}/>
+              <User>
+                <UserGreetings>Olá,</UserGreetings>
+                <UserName>Leandro</UserName>
+              </User>
+          </UserInfo>
+          
+        </UserWrapper>
+        
+      </Header>
     </Container>
   )
 }
