a3cf4452aed087c7bf52ff29283af997cbc11bf7
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-07-13 19:38:10 -0300

    renderizando graficos

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import { HistoryCard } from '../../components/HistoryCard';
-
 import {
   Container,
   Header,
   Title,
-  Content
+  Content,
+  ChartContainer
 } from './styles';
 import { categories } from '../../utils/categories';
+import { useTheme } from 'styled-components';
 
 interface TransactionData {
-  
   type: 'positive' | 'negative';
   name: string;
   amount: string;
@@ -22,12 +24,15 @@ interface TransactionData {
 interface CategoryData {
   key: string;
   name: string;
-  total: string;
+  total: number;
+  totalFormatted: string;
   color: string;
+  percent: string;
 }
 
 export function Resume() {
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+  const theme = useTheme();
 
   async function loadData() {
     const dataKey = '@gofinances:transactions';
@@ -36,6 +41,11 @@ export function Resume() {
 
     const expenses = responseFormatted
       .filter((expense: TransactionData) => expense.type === 'negative');
+
+    const expensesTotal = expenses
+      .reduce((acumullator: number, expense: TransactionData) => {
+        return acumullator + Number(expense.amount);
+      }, 0);
 
     const totalByCategory: CategoryData[] = [];
 
@@ -49,16 +59,22 @@ export function Resume() {
       });
 
       if (categorySum > 0) {
-        const total = categorySum
+        const totalFormatted = categorySum
           .toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'BRL',
           })
+
+
+        const percent = `${(categorySum / expensesTotal * 100).toFixed(0)}%`;
+
         totalByCategory.push({
           key: category.key,
           name: category.name,
           color: category.color,
-          total,
+          total: categorySum,
+          totalFormatted,
+          percent
 
         });
       }
@@ -77,12 +93,29 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
       <Content>
+        <ChartContainer>
+          <VictoryPie
+            data={totalByCategories}
+            colorScale={totalByCategories.map(category => category.color)}
+            style={{
+              labels: { 
+                fontSize: RFValue(18),
+                fontWeight: 'bold',
+                fill: theme.colors.shape 
+              }
+            }}
+            labelRadius={50}
+            x="percent"
+            y="total"
+          />
+        </ChartContainer>
+
         {
           totalByCategories.map(item => (
             <HistoryCard
               key={item.key}
               title={item.name}
-              amount={item.total}
+              amount={item.totalFormatted}
               color={item.color}
             />
           ))
